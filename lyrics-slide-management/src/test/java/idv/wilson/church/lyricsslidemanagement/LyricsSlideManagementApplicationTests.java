62739e00ff40ac8afb09fb35f46b136ef343753f
wilson.wong <keso.haneyou@gmail.com>
2022-08-04 10:21:08 +0800

    Project Init

---
+++ lyrics-slide-management/src/test/java/idv/wilson/church/lyricsslidemanagement/LyricsSlideManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.wilson.church.lyricsslidemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsSlideManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
